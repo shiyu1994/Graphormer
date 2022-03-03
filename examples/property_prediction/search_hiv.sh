bc8750f0f437424efae58bd9ec346dfbd87f5aaa
Yu Shi <shiyu_k1994@qq.com>
2022-03-03 12:51:10 +0000

    adjust #GPU per process

--- examples/property_prediction/search_hiv.sh
+++ examples/property_prediction/search_hiv.sh
@@ -26,11 +26,17 @@ epoch=$6
 batch_size=$7
 warmup_percentage=$8
 num_workers_per_gpu=$9
+num_gpu_per_process=${10}
 
 gpu_id=0
 
+if [[ "${num_gpu_per_process}" != "1" && "${num_gpu_per_process}" != "2" && "${num_gpu_per_process}" != "4" && "${num_gpu_per_process}" != "8" && "${num_gpu_per_process}" != "16" ]]; then
+    echo "Unsupported num_gpu_per_process ${num_gpu_per_process}"
+    exit
+fi
+
 echo "============================= Running a simple task to download MolHIV data ============================="
-bash get_hiv_data.sh
+#bash get_hiv_data.sh
 echo "============================= Finished ============================="
 
 for lr in 2e-4 8e-4 4e-5 8e-5
@@ -41,9 +47,14 @@ do
         do
             for flag_mag in 0.001 0.01 0.1 0 0.0001
             do
-                bash hiv_pre.sh 1 ${epoch} ${batch_size} ${warmup_percentage} ${lr} ${flag_m} ${flag_step_size} ${flag_mag} ${exp_dir} ${base_or_large} ${postln_or_preln} ${gpu_id} ${root} ${num_workers_per_gpu} &
-                echo "Dispatching to GPU ${gpu_id}"
-                gpu_id=$((gpu_id+1))
+                gpu_ids="${gpu_id}"
+                for (( c=1; c<num_gpu_per_process; c++ ))
+                do
+                    gpu_ids+=",$((gpu_id + c))"
+                done
+                bash hiv_pre.sh ${num_gpu_per_process} ${epoch} ${batch_size} ${warmup_percentage} ${lr} ${flag_m} ${flag_step_size} ${flag_mag} ${exp_dir} ${base_or_large} ${postln_or_preln} ${gpu_ids} ${root} ${num_workers_per_gpu} &
+                echo "Dispatching to GPU(s) ${gpu_ids}"
+                gpu_id=$((gpu_id+num_gpu_per_process))
                 if [[ "${gpu_id}" == "${num_gpu}" || "${gpu_id}" == "16" ]]; then
                     echo "Waiting"
                     wait
